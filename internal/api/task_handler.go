92b3c90ee1c408953f1e44b2d59289d4917e665b
Shauryan10 <shindhushauryan@gmail.com>
2026-07-23 20:55:48 +0530

    added a task post req

--- internal/api/task_handler.go
+++ internal/api/task_handler.go
@@ -1,7 +1,50 @@
 package api
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"net/http"
+
+	database "github.com/Shauryan10/GoFlow/internal/databases"
+	"github.com/Shauryan10/GoFlow/internal/models"
+	"github.com/gin-gonic/gin"
+)
 
 func CreateTask(c *gin.Context) {
 
+	var req CreateTaskRequest
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+		fmt.Println(err)
+
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	fmt.Println("Received:", req.Name)
+
+	task := models.Task{
+		Name:   req.Name,
+		Status: "PENDING",
+	}
+
+	result := database.DB.Create(&task)
+
+	if result.Error != nil {
+
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": result.Error.Error(),
+		})
+
+		return
+	}
+
+	response := TaskResponse{
+		ID:     task.ID,
+		Name:   task.Name,
+		Status: task.Status,
+	}
+
+	c.JSON(http.StatusCreated, response)
 }
