dd8d452929b1df592ae13809ea6d792e7742f607
Shauryan10 <shindhushauryan@gmail.com>
2026-08-13 19:58:40 +0530

    setting up docker compose

--- config/config.go
+++ config/config.go
@@ -1,7 +1,6 @@
 package config
 
 import (
-	"log"
 	"os"
 
 	"github.com/joho/godotenv"
@@ -17,11 +16,10 @@ type Config struct {
 }
 
 func LoadConfig() *Config {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env  file")
+	// Load .env when it exists.
+	// In Docker, environment variables are supplied directly.
+	_ = godotenv.Load()
 
-	}
 	return &Config{
 		DBHost:     os.Getenv("DB_HOST"),
 		DBPort:     os.Getenv("DB_PORT"),
